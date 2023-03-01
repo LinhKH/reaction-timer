7b2de270eabaa9fa5ea68f625c48d0e41b5b083a
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-03-01 15:42:54 +0700

    Folder Structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Jobs from '../views//jobs/Jobs.vue'
 
 const routes = [
     {
@@ -15,7 +16,12 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: About
-    }
+    },
+    {
+        path: '/jobs',
+        name: 'Jobs',
+        component: Jobs
+    },
 ]
 
 const router = createRouter({
