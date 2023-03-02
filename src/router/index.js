8740cff049a56f2e069e5fce5d2de58793d3223e
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-03-02 12:27:40 +0700

    Setup project-planner

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-// import About from '../views/About.vue'
-import Jobs from '../views//jobs/Jobs.vue'
-import JobDetails from '../views//jobs/JobDetails.vue'
-import NotFound from '../views/NotFound.vue'
 
 const routes = [
     {
@@ -11,33 +7,6 @@ const routes = [
         name: 'Home',
         component: Home
     },
-    {
-        path: '/about',
-        name: 'About',
-        component: () => import('../views/About.vue')
-    },
-    {
-        path: '/jobs',
-        name: 'Jobs',
-        component: Jobs
-    },
-    {
-        path: '/jobs/:id',
-        name: 'JobDetails',
-        component: JobDetails,
-        props: true
-    },
-    // redirect
-    {
-        path: '/all-jobs',
-        redirect: '/jobs'
-    },
-    // 404 catchall
-    {
-        path: '/:catchAll(.*)',
-        name: 'NotFound',
-        component: NotFound
-    }
 ]
 
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,13 @@
 <template>
     <div class="home">
-        <h1>Homepage</h1>
-        <HelloWorld></HelloWorld>
+        Home
     </div>
 </template>
   
 <script>
-import HelloWorld from "../components/HelloWorld.vue";
+
 export default {
     name: 'Home',
-    components: {
-        HelloWorld
-    }
+    components: {}
 }
 </script>
