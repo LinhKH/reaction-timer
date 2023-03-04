4466e506d6b3b02483f0d1df5471c60e7bd88c4f
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-03-04 12:16:40 +0700

    Showing Filtered Projects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="home">
+        <FilterNav :current="current" @filterChange="current = $event" />
         <div v-if="projects.length">
-            <div v-for="project in projects" :key="project.id">
+            <div v-for="project in filteredProjects" :key="project.id">
                 <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
             </div>
         </div>
@@ -10,13 +11,15 @@
   
 <script>
 import SingleProject from '../components/SingleProject.vue'
+import FilterNav from '../components/FilterNav.vue'
 
 export default {
     name: 'Home',
-    components: { SingleProject },
+    components: { SingleProject, FilterNav },
     data() {
         return {
             projects: [],
+            current: 'all',
         };
     },
     mounted() {
@@ -37,6 +40,17 @@ export default {
             })
             p.complete = !p.complete
             // console.log(p)
+        }
+    },
+    computed: {
+        filteredProjects() {
+            if (this.current === 'completed') {
+                return this.projects.filter(project => project.complete)
+            }
+            if (this.current === 'ongoing') {
+                return this.projects.filter(project => !project.complete)
+            }
+            return this.projects
         }
     }
 }
