a0dae1a13c11fa577ecc29650fd910eeeab6924c
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-03-05 15:28:52 +0700

    Composition API Basics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,29 @@
 <template>
     <div class="home">
-        <FilterNav :current="current" @filterChange="current = $event" />
-        <div v-if="projects.length">
-            <div v-for="project in filteredProjects" :key="project.id">
-                <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
-            </div>
-        </div>
+        <h1>Home</h1>
+        <p>My name is {{ name }} and my age is {{ age }}</p>
+        <button @click="handleClick">click me</button>
     </div>
 </template>
   
 <script>
-import SingleProject from '../components/SingleProject.vue'
-import FilterNav from '../components/FilterNav.vue'
 
 export default {
     name: 'Home',
-    components: { SingleProject, FilterNav },
-    data() {
-        return {
-            projects: [],
-            current: 'all',
-        };
+    setup() {
+        console.log('setup')
+
+        let name = 'mario'
+        let age = 30
+
+        const handleClick = (e) => {
+            console.log('you clicked me')
+        }
+
+        return { name, age, handleClick }
     },
     mounted() {
-        fetch('http://localhost:3000/projects')
-            .then(res => res.json())
-            .then(data => this.projects = data)
-            .catch(err => console.log(err))
-    },
-    methods: {
-        handleDelete(id) {
-            this.projects = this.projects.filter(project => {
-                return project.id !== id
-            })
-        },
-        handleComplete(id) {
-            let p = this.projects.find(project => {
-                return project.id === id
-            })
-            p.complete = !p.complete
-            // console.log(p)
-        }
-    },
-    computed: {
-        filteredProjects() {
-            if (this.current === 'completed') {
-                return this.projects.filter(project => project.complete)
-            }
-            if (this.current === 'ongoing') {
-                return this.projects.filter(project => !project.complete)
-            }
-            return this.projects
-        }
+        console.log('mounted')
     }
 }
 </script>
