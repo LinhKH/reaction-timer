3bac2be3ff965ffbe4c02427ea4c2bce1f4a99f4
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-03-07 15:55:56 +0700

    Creating Another Composable

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="post-list">
         <div v-for="post in posts" :key="post.id">
-            <!-- <h3>{{ post.title }}</h3> -->
             <SinglePost :post="post" />
         </div>
     </div>
 </template>
   
 <script>
-import { onMounted, onUnmounted, onUpdated } from 'vue'
+// import { onMounted, onUnmounted, onUpdated } from 'vue'
 // component imports
 import SinglePost from './SinglePost.vue'
 
@@ -16,9 +15,7 @@ export default {
     props: ['posts'],
     components: { SinglePost },
     setup() {
-        onMounted(() => console.log('component mounted'))
-        onUnmounted(() => console.log('component unmounted'))
-        onUpdated(() => console.log('component updated'))
+
     }
 }
 </script>
