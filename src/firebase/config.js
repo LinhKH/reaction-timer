49089e9c05667307333671a77c967bc829d25642
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-03-15 16:12:22 +0700

    Firebase Setup (& Reusing Firebase Projects)

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC1Ls7wkzq1nAEc69YytsBsm3yfS0cyI5o",
+    authDomain: "udemy-vue-firebase-sites-2e4bc.firebaseapp.com",
+    projectId: "udemy-vue-firebase-sites-2e4bc",
+    storageBucket: "udemy-vue-firebase-sites-2e4bc.appspot.com",
+    messagingSenderId: "588079349594",
+    appId: "1:588079349594:web:8b30f08f8f265b1cd93d63"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
